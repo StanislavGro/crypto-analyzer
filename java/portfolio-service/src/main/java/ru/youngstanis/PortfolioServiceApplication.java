ce4df16691374afc5cf6d1bea7b89e5a35b8ef3c
Youngstanislav <stanislav.skii2000@mail.ru>
2025-03-01 18:00:58 +0700

    Обновил проект, удалил не нужные (тестовые модули) и добавил первый нужный модуль portfolio-service, внедрил туда Spring Boot и протестил работу

---
+++ java/portfolio-service/src/main/java/ru/youngstanis/PortfolioServiceApplication.java
@@ -0,0 +1,11 @@
+package ru.youngstanis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioServiceApplication.class, args);
+    }
+}
